8f40190ce158ce04eebd8fc53b4ff12b22cda6d2
Rohit Vishwakarma <inbox.rohitv@gmail.com>
2025-11-01 13:32:44 +0000

    optimization flags

--- benchmarks/bench_library.cpp
+++ benchmarks/bench_library.cpp
@@ -1,10 +1,10 @@
 #include "library.h"
 #include <benchmark/benchmark.h>
-
+namespace bm = benchmark;
 static void BM_fn1(benchmark::State& state) {
-	for (auto _ : state) {
-		fn1(10, 20);
-	}
+	int32_t a = std::rand(), b = std::rand(), c = 0;
+	for (auto _ : state)
+		bm::DoNotOptimize(c = a + b);
 }
 BENCHMARK(BM_fn1);
 BENCHMARK_MAIN();
